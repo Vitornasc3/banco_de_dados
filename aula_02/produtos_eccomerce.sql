1f79ad38e4a06300f17366587f98b6856059b9f6
Vitor Ferreira <vitornasc673@gmail.com>
2023-12-04 12:19:10 -0300

    segunda atividade - eccomerce

---
+++ aula_02/produtos_eccomerce.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE db_soneca_games;
+
+USE db_soneca_games;
+
+CREATE TABLE tb_produtos(
+id bigint AUTO_INCREMENT,
+nome varchar(255) NOT NULL,
+categoria varchar(255),
+plataforma varchar(255),
+preco decimal(6,2) NOT NULL,
+PRIMARY KEY(id)
+);
+
+INSERT INTO tb_produtos(nome, categoria, plataforma, preco)
+VALUES ("Devil May Cry 5", "Jogo", "PS4", 79.00),
+("Dual-Sense", "acessóio", "PS5", 379.90),
+("The Legend of Zelda: Tears of the Kingdon", "jogo", "Nintendo Switch", 309.00),
+("Baldur's Gate", "jogo", "PC", 180.00),
+("Diablo 4", "jogo", "XBOX Series X", 379.90),
+("Call of Duty - Modern Warfare 3", "jogo", "PS5", 89.90),
+("God of War: Ragnarok", "jogo", "PS5", 189.90),
+("Headset Gamer Hyper X", "acessório", "Multiplataforma", 179.00);
+
+SELECT * FROM tb_produtos;
+
+SELECT * FROM tb_produtos WHERE preco > 180.00;
+
+SELECT * FROM tb_produtos WHERE preco <= 180.00;
+
+UPDATE tb_produtos SET categoria = "acessório" WHERE id = 2;
